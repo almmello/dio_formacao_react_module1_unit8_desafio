01dee33e996a12502d9bb62e46a730686c5a49c4
Alexandre <almmello@gmail.com>
2022-11-06 19:20:18 -0300

    Interface Adjustment Made some improvements in the UI. Added the percentage calculation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import Input from './components/Input';
 import Button from './components/Button';
+import ButtonDouble from './components/ButtonDouble';
 
 import { Container, Content, Row } from './styles';
 import { useState } from 'react';
@@ -84,6 +85,17 @@ const App = () => {
   }
 
 
+  const handlePercent = (oriEqual) => {
+    
+    const result = Number(currentNumber) / 100;
+    setCurrentNumber(String(result))
+    setIsClear(false)
+    setFirstNumber('0');
+    setIsFirstClear(true)
+    setNewOperation(false)
+    
+  }
+
   const handleEquals = () => {
 
     if (operation !== '') {
@@ -105,28 +117,32 @@ const App = () => {
       <Content>
         <Input value={currentNumber} />
         <Row>
-          <Button label="x" onClick={() => handleOperations(false, '*')}/>
-          <Button label="/" onClick={() => handleOperations(false, '/')}/>
-          <Button label="c" onClick={handleOnClear} />
-          <Button label="." />
+          <ButtonDouble label="C" onClick={handleOnClear} />
+          <Button label="%" onClick={() => handlePercent(false)}/>
+          <Button label="/" onClick={() => handleOperations(false, '/')}/>          
         </Row>
         <Row>
           <Button label="7" onClick={() => handleAddNumber('7')} />
           <Button label="8" onClick={() => handleAddNumber('8')} />
           <Button label="9" onClick={() => handleAddNumber('9')} />
-          <Button label="-" onClick={() => handleOperations(false, '-')} />
+          <Button label="x" onClick={() => handleOperations(false, '*')} />
         </Row>
         <Row>
           <Button label="4" onClick={() => handleAddNumber('4')} />
           <Button label="5" onClick={() => handleAddNumber('5')} />
           <Button label="6" onClick={() => handleAddNumber('6')} />
-          <Button label="+" onClick={() => handleOperations(false, '+')} />
+          <Button label="-" onClick={() => handleOperations(false, '-')} />
         </Row>
         <Row>
           <Button label="1" onClick={() => handleAddNumber('1')} />
           <Button label="2" onClick={() => handleAddNumber('2')} />
           <Button label="3" onClick={() => handleAddNumber('3')} />
-          <Button label="=" onClick={() => handleEquals()} />
+          <Button label="+" onClick={() => handleOperations(false, '+')} />
+        </Row>
+        <Row>
+          <ButtonDouble label="0" onClick={() => handleAddNumber('0')} />
+          <Button label="." onClick={() => handleAddNumber('.')}/>
+          <Button label="=" onClick={() => handleEquals()}/>          
         </Row>
       </Content>
     </Container>
---
+++ src/components/ButtonDouble/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px 41px;
+    border: 1px solid #ffffff;
+    background-color: #C1C6C8;
+    color: #54585A;
+    font-size: 24px;
+    font-weight: 700;
+    font-family: 'Roboto', sans-serif;
+    flex: 2;
+    
+    &:hover {
+        opacity: 0.6;
+    }
+`
